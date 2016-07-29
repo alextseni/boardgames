d9cd1a0060008043a7bd60285dff51b5f524f1ba
Alexandra Tseniklidou <alexandra.tseniklidou@agileactors.com>
2016-07-29 17:32:21 +0300

    graphical changes 2
    color changes

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -15,7 +15,7 @@ export const Game = (props) => (
     <div className="progress" id={classes.prog}>
       <div className="progress-bar" role="progressbar"
       style={{width:Math.round((100-(((props.game.pieces.filter((p)=> p=='marble' || p=='selected').length-1))/(props.game.allMarbles-1))*100)) + '%'}}>
-        {Math.round((100-(((props.game.pieces.filter((p)=> p=='marble' || p=='selected').length-1))/(props.game.allMarbles-1))*100)) + '%'}
+           {Math.round((100-(((props.game.pieces.filter((p)=> p=='marble' || p=='selected').length-1))/(props.game.allMarbles-1))*100)) + '%'}
       </div>
     </div>
   </div>
@@ -25,7 +25,7 @@ export const Game = (props) => (
     <table className={classes.board}>
      <tbody>
        {props.game.pieces.slice(0,6).map( (p, k) => {return <tr>
-       {props.game.pieces.slice(k*6,k*6+6).map( (piece, key) => {return <td className= {classes[piece]}
+       {props.game.pieces.slice(k*6,k*6+6).map( (piece, key) => {return <td className= {classes[piece]} id= {classes[props.game.text]}
            onMouseDown={(ev)=> {isMouseDown = true; ev.preventDefault(); props.markPiece(k*6+key);}}
            onMouseOver ={()=> { if (isMouseDown) props.markPiece(k*6+key);}}
 
